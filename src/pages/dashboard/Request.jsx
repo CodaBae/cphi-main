493df0adc333818e7236b6a5ad43ff8807df4aff
Arcee69 <olawalearcee@gmail.com>
2024-11-01 00:23:19 +0100

    Hot Fix

--- src/pages/dashboard/Request.jsx
+++ src/pages/dashboard/Request.jsx
@@ -1,5 +1,5 @@
 import React, { useState, Fragment, useEffect } from 'react'
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, getDoc, getDocs } from 'firebase/firestore'
 import { AiOutlineClose } from 'react-icons/ai'
 import { CgSpinner } from 'react-icons/cg'
 import { toast } from 'react-toastify'
@@ -8,9 +8,10 @@ import { db } from '../../firebase-config'
 
 const Request = ({ handleClose, data, userDetails }) => {
     const [loading, setLoading] = useState(false)
+    const [rewards, setRewards] = useState([])
 
     console.log(data, "simple")
-    console.log(userDetails, "userDetails")
+
 
     const submitForm = async () => {
         setLoading(true);
@@ -30,11 +31,42 @@ const Request = ({ handleClose, data, userDetails }) => {
         }
     };
     
+    const getRewards = async () => {
+        try {
+            const rewardRef = collection(db, "rewards")
+            const querySnapshot = await getDocs(rewardRef)
 
+            const data = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+
+            setRewards(data)
+
+        } catch (err) {
+            console.log("Error getting Rewards", err)
+        }
+    }
+
+    console.log(rewards, "paski")
+
+    useEffect(() => {
+        getRewards()
+    }, [])
+
+    const getRewardDescription = () => {
+        const currentLength = data?.length || 0;
+
+        const reward = rewards.find(({ range }) => 
+            currentLength >= parseInt(range.from) && currentLength <= parseInt(range.to)
+        );
+
+        return reward ? reward.description : 'No rewards available for this range';
+    };
 
 
   return (
-    <div className='bg-[#fff] w-[668px] h-[508px] flex flex-col p-6 mt-[100px] overflow-y-scroll gap-[32px] rounded-lg'>
+    <div className='bg-[#fff] w-[668px] h-[508px] flex flex-col p-6 mt-[100px] overflow-y-scroll relative gap-[32px] rounded-lg'>
         <div className='flex items-center justify-between'>
             <p className='font-semibold text-[#1C1C1E] font-sans text-[18px]'>Request Reward</p>
             <AiOutlineClose className='text-[#817F9B] cursor-pointer text-[24px]' onClick={handleClose} />
@@ -46,23 +78,19 @@ const Request = ({ handleClose, data, userDetails }) => {
                 <p className='font-sans text-[#1C1C1C] font-bold text-[24px]'>{data?.length}</p>
             </div>
 
-            <div className='w-full flex flex-col gap-[32px]'>
+            <div className='w-full flex flex-col  gap-[32px]'>
                 <p className='font-mulish text-[#1C1C1E] text-[17px]'>
-                    Bluetooth Wireless Earbuds: Enjoy quality sound on the go with sleek, wireless audio.
-                    Smart Home Assistant Speaker: Control your home’s lighting, get news updates, 
-                    or play music with ease using devices like Google Home or Amazon Echo.
-                    32" HD Smart TV: Elevate your streaming experience with a new HD screen for your favorite shows and movies.
-                    High-Speed USB Flash Drive (128GB): Securely store important files with ample space in a compact design.
+                    {getRewardDescription()}
                 </p>
                
+            </div>
                 <button
-                    className='flex items-center justify-center gap-2 w-full rounded-lg h-[45px] p-2 bg-[#2D84FF]'
+                    className='flex items-center absolute bottom-8 justify-center gap-2 w-11/12 rounded-lg h-[45px] p-2 bg-[#2D84FF]'
                     type='submit'
                     onClick={submitForm}
                 >
                     <p className='text-[#fff] font-sans font-semibold text-sm'>{loading ? <CgSpinner className=" animate-spin text-lg " /> : 'Request Reward'}</p>
                 </button>
-            </div>
         </div>
 
     </div>
